a7b927617dd840d4e5082ee4e98f2337e5961718
Alex Cruz <axtscz@gmail.com>
2017-04-05 14:45:08 -0500

    change styling

--- src/app/Character.js
+++ src/app/Character.js
@@ -7,7 +7,7 @@ export default class Character extends React.Component {
     render() {
         return (
             <div className="character">
-                <h4>{this.props.character.name}</h4>
+                <h4>{this.props.character.name} <span>{this.props.character.gender}</span></h4>
                 <p>{this.props.character.culture}</p>
             </div>
         );
--- src/app/index.js
+++ src/app/index.js
@@ -8,8 +8,8 @@ export default class App extends Component {
         return (
             <div className="container">
                 <h1>WriteThus</h1>
+                <h2>Characters</h2>
                 <div className="item-container">
-                    <h2>Characters</h2>
                     {this.props.characters.map((character, index) => {
                         return <Character character={character} key={index}/>
                     })}
