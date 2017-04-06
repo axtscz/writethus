7d2d561970bca1f1abb8318113796d312c4e774d
Alex Cruz <axtscz@gmail.com>
2017-04-06 17:26:44 -0500

    Added Google Analytics

--- src/template.js
+++ src/template.js
@@ -14,8 +14,17 @@ export default ({body, title, initialState}) => {
       <body>
         <div id="root" class="container">${body}</div>
       </body>
-      
       <script src="/assets/bundle.js"></script>
+      <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+        
+          ga('create', 'UA-61201132-3', 'auto');
+          ga('send', 'pageview');
+        
+      </script>
     </html>
   `;
 };
