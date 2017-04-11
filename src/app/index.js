979b5f9a66d061fc669b963b6e24d4fb17c87ebd
Alex Cruz <axtscz@gmail.com>
2017-04-11 08:12:14 -0500

    Icons and manifest

--- src/app/index.js
+++ src/app/index.js
@@ -2,6 +2,7 @@ import React, {Component, PropTypes} from "react";
 import Character from "./Components/Character";
 import Location from "./Components/Location";
 import Genre from "./Components/Genre";
+import {FormattedNumber, IntlProvider} from "react-intl";
 
 export default class App extends Component {
     render() {
@@ -10,32 +11,36 @@ export default class App extends Component {
         const {location} = this.props;
         const {genre} = this.props;
         const url = "https://writeth.us/" + this.props.seed;
+        const {numCombinations} = this.props;
         return (
-            <div>
-                <h1>WriteThus</h1>
-                <p>Write a story using the following components</p>
-                <a href="/">New</a>
+            <IntlProvider locale="en">
+                <div>
+                    <h1>WriteThus</h1>
+                    <p>Write a story using the following components. There are <FormattedNumber
+                        value={this.props.combinations}/> possible combinations.</p>
+                    <a href="/">New</a>
 
-                <div className="row">
-                    <div className="four columns">
-                        <h3><i className="fa fa-globe" aria-hidden="true"></i> Location</h3>
-                        <Location location={location}/>
+                    <div className="row">
+                        <div className="four columns">
+                            <h3><i className="fa fa-globe" aria-hidden="true"></i> Location</h3>
+                            <Location location={location}/>
+                        </div>
+                        <div className="four columns">
+                            <h3><i className="fa fa-user" aria-hidden="true"></i> Characters</h3>
+                            {this.props.characters.map((character, index) => {
+                                return <Character character={character} key={index}/>
+                            })}
+                        </div>
+                        <div className="four columns">
+                            <h3><i className="fa fa-book" aria-hidden="true"></i> Genre</h3>
+                            <Genre genre={genre}/>
+                        </div>
                     </div>
-                    <div className="four columns">
-                        <h3><i className="fa fa-user" aria-hidden="true"></i> Characters</h3>
-                        {this.props.characters.map((character, index) => {
-                            return <Character character={character} key={index}/>
-                        })}
-                    </div>
-                    <div className="four columns">
-                        <h3><i className="fa fa-book" aria-hidden="true"></i> Genre</h3>
-                        <Genre genre={genre}/>
-                    </div>
+
+                    <p>Share this prompt</p>
+                    <input type="text" value={url} disabled/>
                 </div>
-
-                <p>Share this prompt</p>
-                <input type="text" value={url} disabled/>
-            </div>
+            </IntlProvider>
         );
     }
 }
@@ -45,5 +50,6 @@ App.propTypes = {
     location: PropTypes.string,
     genre: PropTypes.string,
     seed: PropTypes.string,
+    combinations: PropTypes.string,
     url: PropTypes.string
 };
